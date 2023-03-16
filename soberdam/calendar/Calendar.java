ded3afa9ca7893c8a526711474d405e12a58a7eb
soberdam <j38903639@gmail.com>
2023-03-16 16:52:45 +0900

    Update printSampleCalender() and Add printing virtual calendar function

--- soberdam/calendar/Calendar.java
+++ soberdam/calendar/Calendar.java
@@ -8,13 +8,33 @@ public class Calendar {
 		return MAX_DAYS[month-1];
 	}
 	
-	public void printSampleCalendar() {
+	public void printSampleCalendar28() {
 		System.out.println("일  월  화  수 목  금 토");
 		System.out.println("--------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
+	}
+	
+	public void printSampleCalendar30() {
+		System.out.println("일  월  화  수 목  금 토");
+		System.out.println("--------------------");
+		System.out.println(" 1  2  3  4  5  6  7");
+		System.out.println(" 8  9 10 11 12 13 14");
+		System.out.println("15 16 17 18 19 20 21");
+		System.out.println("22 23 24 25 26 27 28");
+		System.out.println("29 30");
+	}
+	
+	public void printSampleCalendar31() {
+		System.out.println("일  월  화  수 목  금 토");
+		System.out.println("--------------------");
+		System.out.println(" 1  2  3  4  5  6  7");
+		System.out.println(" 8  9 10 11 12 13 14");
+		System.out.println("15 16 17 18 19 20 21");
+		System.out.println("22 23 24 25 26 27 28");
+		System.out.println("29 30 31");
 	}
 	
 	public static void main(String[] args) {
@@ -32,15 +52,25 @@ public class Calendar {
 			System.out.println("월을 입력하세요.");
 			System.out.print(PROMPT);
 			month = scanner.nextInt();
+			
 			if(month == -1) {
 				break;
 			} else if (month > 12) {
 				continue;
 			}
-			System.out.printf("%d월은 %d일까지 있습니다. \n", month, cal.getMaxDaysOfMonth(month));
+			
+			int maxDay = cal.getMaxDaysOfMonth(month);
+			if(maxDay == 31) {
+				cal.printSampleCalendar31();
+			}else if(maxDay == 30) {
+				cal.printSampleCalendar30();
+			}else {
+				cal.printSampleCalendar28();
+			}
+			
 		}
 		
-		System.out.println("Have a nice day!");
+		System.out.println("Bye~");
 		scanner.close();
 	}
 }
