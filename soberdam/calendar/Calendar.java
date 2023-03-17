8741e65ae990056abb3e2a98a1af3a41a6ee74f7
soberdam <j38903639@gmail.com>
2023-03-17 13:32:13 +0900

    Add getWeekday() method

--- soberdam/calendar/Calendar.java
+++ soberdam/calendar/Calendar.java
@@ -19,12 +19,8 @@ public class Calendar {
 			return MAX_DAYS[month - 1];
 		}
 	}
-
-	public void printCalendar(int year, int month) {
-		System.out.printf("   <<%d년 %d월>>\n", year, month);
-		System.out.println("SU MO TU WE TH FR SA");
-		System.out.println("----------------------");
-		int maxDay = getMaxDaysOfMonth(year, month); // month의 일수
+	public int getWeekday(int year, int month) {
+		
 		int firstWeekday = 0; // 0년 1월 1일의 요일 = 일요일
 		int weekday = 0; // month의 1일의 요일
 
@@ -40,7 +36,17 @@ public class Calendar {
 		for (int i = 1; i < month; i++) {
 			weekday = (weekday + (getMaxDaysOfMonth(year, i) % 7)) % 7;
 		} // month의 1일 요일 구하기
+		
+		return weekday;
+	}
 
+	public void printCalendar(int year, int month) {
+		System.out.printf("   <<%d년 %d월>>\n", year, month);
+		System.out.println("SU MO TU WE TH FR SA");
+		System.out.println("----------------------");
+		int maxDay = getMaxDaysOfMonth(year, month); // month의 일수
+		int weekday = getWeekday(year, month); // month의 1일 요일
+		
 		System.out.print("   ".repeat(weekday));
 		for (int i = 1; i <= maxDay; i++) {
 			System.out.printf("%3d", i);
